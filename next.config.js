53323eba2bdab960ff54499697712ef3ada90f12
Mirza Ahtsham <mirza.ahtsham786@outlook.com>
2025-03-01 13:36:23 +0500

    Fix The Next Build Issue and Now Try to Deploy this code.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      unoptimized: true, // Required for Cloudflare Pages
-    },
-  };
-  
-  module.exports = nextConfig;
+  images: {
+    unoptimized: true, // Required for Cloudflare Pages
+  },
+  output: 'export', // Ensures static export for Cloudflare Pages
+};
+
+module.exports = nextConfig;
